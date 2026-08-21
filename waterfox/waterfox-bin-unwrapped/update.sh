d3f3f16cb5189305af9b86bc5780550b6c0e276f
Winni Neessen <wn@neessen.dev>
2026-08-21 14:38:55 +0200

    Updated update script

--- waterfox/waterfox-bin-unwrapped/update.sh
+++ waterfox/waterfox-bin-unwrapped/update.sh
@@ -3,7 +3,7 @@
 
 set -eou pipefail
 
-latestVersion=$(curl ${GITHUB_TOKEN:+-H "Authorization: Bearer $GITHUB_TOKEN"} -sL https://api.github.com/repos/BrowserWorks/Waterfox/releases/latest | jq -r '.tag_name')
+latestVersion=$(curl -sL https://api.github.com/repos/BrowserWorks/Waterfox/releases/latest | jq -r '.tag_name')
 currentVersion=$(nix eval --raw .#waterfox-bin-unwrapped.version)
 
 echo "latest  version: $latestVersion"
@@ -17,9 +17,5 @@ fi
 hash_linux=$(nix-prefetch-url https://cdn.waterfox.com/waterfox/releases/${latestVersion}/Linux_x86_64/waterfox-${latestVersion}.tar.bz2)
 hash_linux=$(nix --extra-experimental-features nix-command hash convert "sha256:$hash_linux")
 echo Linux: $hash_linux
-
-hash_darwin=$(nix-prefetch-url https://cdn.waterfox.com/waterfox/releases/${latestVersion}/Darwin_x86_64-aarch64/Waterfox%20${latestVersion}.dmg --name waterfox-${latestVersion}.dmg)
-hash_darwin=$(nix --extra-experimental-features nix-command hash convert "sha256:$hash_darwin")
-echo Darwin: $hash_darwin
 
 #update-source-version waterfox-bin-unwrapped $latestVersion $hash --system="x86_64-linux" --ignore-same-version
